a4e355b93848e39207b191f41e65dc4ca536960a
yuuya1986 <yuuyagit@gmail.com>
2011-12-18 22:20:27 +0900

    変数の初期化処理削除

--- magicSquare/controller/index.php
+++ magicSquare/controller/index.php
@@ -1,13 +1,2 @@
 <?php
-
-$overRecord[0]   = '';
-$overRecord[1]   = '';
-$overRecord[2]   = '';
-$middleRecord[0] = '';
-$middleRecord[1] = '';
-$middleRecord[2] = '';
-$underRecord[0]  = '';
-$underRecord[1]  = '';
-$underRecord[2]  = '';
-
 require_once '../view/formTemplate.php';
