45ce15f73eb97ba64b2216f28c7d8029b89c1fa0
yuuya1986 <yuuyagit@gmail.com>
2011-12-18 22:55:47 +0900

    エスケープ処理追加

--- magicSquare/view/correctAnswerBody.php
+++ magicSquare/view/correctAnswerBody.php
@@ -1,3 +1,5 @@
+<?php require_once '../model/util.php'; ?>
+
 <p>
 <h1>正解！！</h1>
 </p>
@@ -5,19 +7,19 @@
 <p>
 <table border="1" width="130" height="130">
     <tr align="center">
-        <td><?php echo $over[0]; ?></td>
-        <td><?php echo $over[1]; ?></td>
-        <td><?php echo $over[2]; ?></td>
+        <td><?php echo h($over[0]); ?></td>
+        <td><?php echo h($over[1]); ?></td>
+        <td><?php echo h($over[2]); ?></td>
     </tr>
     <tr align="center">
-        <td><?php echo $middle[0]; ?></td>
-        <td><?php echo $middle[1]; ?></td>
-        <td><?php echo $middle[2]; ?></td>
+        <td><?php echo h($middle[0]); ?></td>
+        <td><?php echo h($middle[1]); ?></td>
+        <td><?php echo h($middle[2]); ?></td>
     </tr>
     <tr align="center">
-        <td><?php echo $under[0]; ?></td>
-        <td><?php echo $under[1]; ?></td>
-        <td><?php echo $under[2]; ?></td>
+        <td><?php echo h($under[0]); ?></td>
+        <td><?php echo h($under[1]); ?></td>
+        <td><?php echo h($under[2]); ?></td>
     </tr>
 </table>
 </p>
