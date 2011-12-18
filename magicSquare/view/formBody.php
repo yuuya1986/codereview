1125f5fb81ece5d07a24883f2d5133145df5a693
yuuya1986 <yuuyagit@gmail.com>
2011-12-18 22:56:15 +0900

    エスケープ処理追加

--- magicSquare/view/formBody.php
+++ magicSquare/view/formBody.php
@@ -1,3 +1,5 @@
+<?php require_once '../model/util.php'; ?>
+
 <p>
 <h1>魔方陣が空前の大ブームです</h1>
 縦、横、斜めの列の合計値を揃えましょう<br />
@@ -8,19 +10,19 @@
 <form method="POST" action="../controller/exec.php">
     <table border="1" width="130" height="130">
         <tr>
-            <td><input type="text" name="overRecord[]" size="3" maxlength="4" value="<?php if (!empty($over[0])) echo $over[0]; ?>"></td>
-            <td><input type="text" name="overRecord[]" size="3" maxlength="4" value="<?php if (!empty($over[1])) echo $over[1]; ?>"></td>
-            <td><input type="text" name="overRecord[]" size="3" maxlength="4" value="<?php if (!empty($over[2])) echo $over[2]; ?>"></td>
+            <td><input type="text" name="overRecord[]" size="3" maxlength="4" value="<?php if (!empty($over[0])) echo h($over[0]); ?>"></td>
+            <td><input type="text" name="overRecord[]" size="3" maxlength="4" value="<?php if (!empty($over[1])) echo h($over[1]); ?>"></td>
+            <td><input type="text" name="overRecord[]" size="3" maxlength="4" value="<?php if (!empty($over[2])) echo h($over[2]); ?>"></td>
         </tr>
         <tr>
-            <td><input type="text" name="middleRecord[]" size="3" maxlength="4" value="<?php if (!empty($middle[0])) echo $middle[0]; ?>"></td>
-            <td><input type="text" name="middleRecord[]" size="3" maxlength="4" value="<?php if (!empty($middle[0])) echo $middle[1]; ?>"></td>
-            <td><input type="text" name="middleRecord[]" size="3" maxlength="4" value="<?php if (!empty($middle[0])) echo $middle[2]; ?>"></td>
+            <td><input type="text" name="middleRecord[]" size="3" maxlength="4" value="<?php if (!empty($middle[0])) echo h($middle[0]); ?>"></td>
+            <td><input type="text" name="middleRecord[]" size="3" maxlength="4" value="<?php if (!empty($middle[0])) echo h($middle[1]); ?>"></td>
+            <td><input type="text" name="middleRecord[]" size="3" maxlength="4" value="<?php if (!empty($middle[0])) echo h($middle[2]); ?>"></td>
         </tr>
         <tr>
-            <td><input type="text" name="underRecord[]" size="3" maxlength="4" value="<?php if (!empty($under[0])) echo $under[0]; ?>"></td>
-            <td><input type="text" name="underRecord[]" size="3" maxlength="4" value="<?php if (!empty($under[1])) echo $under[1]; ?>"></td>
-            <td><input type="text" name="underRecord[]" size="3" maxlength="4" value="<?php if (!empty($under[2])) echo $under[2]; ?>"></td>
+            <td><input type="text" name="underRecord[]" size="3" maxlength="4" value="<?php if (!empty($under[0])) echo h($under[0]); ?>"></td>
+            <td><input type="text" name="underRecord[]" size="3" maxlength="4" value="<?php if (!empty($under[1])) echo h($under[1]); ?>"></td>
+            <td><input type="text" name="underRecord[]" size="3" maxlength="4" value="<?php if (!empty($under[2])) echo h($under[2]); ?>"></td>
         </tr>
     </table>
 </p>
